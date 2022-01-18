39c2b77df91ab173f5391733c3e3f55eb437dbf6
giovannic <gc1610@ic.ac.uk>
2022-01-18 10:00:56 +0000

    Code and data for vector extraction

---
+++ vector_extract.R
@@ -0,0 +1,28 @@
+admin0 <- readRDS('Z:/Eradication/rds/admin0.RDS')
+admin0 <- admin0[which(admin0$ISO=='SEN'), ]
+sf::st_geometry(admin0)
+
+props <- rep(NA, 3)
+species <- c('arabiensis', 'funestus', 'gambiae_ss')
+
+for (i in seq_along(species)) {
+  s <- species[[i]]
+  dat <- terra::rast(paste0('data/vectors/2010_Anopheles_', s, '.tif'))
+  dat <- terra::project(dat, "EPSG:4326", method = "bilinear")
+  dat <- raster::raster(dat)
+  
+  props[[i]] <- raster::extract(
+    dat,
+    admin0,
+    method='simple',
+    fun=mean,
+    na.rm=T,
+    weights=F
+  )
+}
+
+df <- data.frame(
+  species = species,
+  props = props,
+  normalised_props = props / sum(props)
+)
